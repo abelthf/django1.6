d501e8e45e51901b966fca5dcb458efdea8243d3
abelthf <abel.huanca@upeu.pe>
2014-01-19 12:51:50 -0500

    fdsa

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
@@ -12,9 +11,8 @@ urlpatterns = patterns('',
     # url(r'^$', '{{ project_name }}.views.home', name='home'),
     # url(r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^$', 'project_name.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
